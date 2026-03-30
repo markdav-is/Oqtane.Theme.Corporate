eb8fc3cfd561ea2d3c04e9a6e6bccb21df828713
mark <ma@rkdav.is>
2026-03-29 19:23:16 -0700

    Update solution for VS2022, add local nuget.exe, cleanup scripts
    Updated Oqtane.Theme.Corporate.sln for Visual Studio 2022 compatibility and added a Solution Items folder with nuget.exe. Removed debug.cmd and updated release.cmd to use the local nuget.exe for packaging. Added nuget.exe binary to the repository.

--- Package/release.cmd
+++ Package/release.cmd
@@ -1,3 +1,2 @@
 del "*.nupkg"
-"..\..\oqtane.framework\oqtane.package\nuget.exe" pack Oqtane.Theme.Corporate.nuspec 
-XCOPY "*.nupkg" "..\..\oqtane.framework\Oqtane.Server\Packages\" /Y
+"..\nuget.exe" pack Oqtane.Theme.Corporate.nuspec
